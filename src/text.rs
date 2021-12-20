2c57b6d6f612b3b03d9c0c242682a46624349998
Gleb Vodovozov <vodovozovge@gmail.com>
2021-12-20 21:21:21 +0800

    made image_to_raster_lines public

--- src/text.rs
+++ src/text.rs
@@ -1,10 +1,10 @@
 //! Easy-to-use text and image compositing and rasterization for use with Brother QL printers
 
-use std::path::PathBuf;
-use std::fs;
-use rusttype::{ Scale, Point, Font };
-use image::{ DynamicImage, Luma };
 use crate::printer::constants::Label;
+use image::{DynamicImage, Luma};
+use rusttype::{Font, Point, Scale};
+use std::fs;
+use std::path::PathBuf;
 
 type XY<T> = Point<T>;
 
@@ -32,12 +32,24 @@ impl<'a> ResizedText<'a> {
         let glyphs = loop {
             let scale = Scale::uniform(font_size);
             let v_metrics = font.v_metrics(scale);
-            let glyphs: Vec<_> = font.layout(text, scale, Point { x: 0.0, y: v_metrics.ascent }).collect();
+            let glyphs: Vec<_> = font
+                .layout(
+                    text,
+                    scale,
+                    Point {
+                        x: 0.0,
+                        y: v_metrics.ascent,
+                    },
+                )
+                .collect();
 
             let width = calc_text_width(&glyphs);
             if width < max_width {
                 let height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
-                rendered_size = XY { x: width, y: height };
+                rendered_size = XY {
+                    x: width,
+                    y: height,
+                };
                 break glyphs;
             }
             font_size -= 1.0;
@@ -50,15 +62,19 @@ impl<'a> ResizedText<'a> {
     }
 }
 
-fn draw_glyphs(image: &mut image::GrayImage, glyphs: &[rusttype::PositionedGlyph], offset: XY<i32>, invert: bool) {
+fn draw_glyphs(
+    image: &mut image::GrayImage,
+    glyphs: &[rusttype::PositionedGlyph],
+    offset: XY<i32>,
+    invert: bool,
+) {
     for glyph in glyphs {
         if let Some(bounding_box) = glyph.pixel_bounding_box() {
             // Draw the glyph into the image per-pixel by using the draw closure
             glyph.draw(|x, y, v| {
                 let color = if invert {
                     (255.0 * v) as u8
-                }
-                else {
+                } else {
                     255 - (255.0 * v) as u8
                 };
 
@@ -67,14 +83,14 @@ fn draw_glyphs(image: &mut image::GrayImage, glyphs: &[rusttype::PositionedGlyph
                     (x as i32 + bounding_box.min.x + offset.x) as u32,
                     (y as i32 + bounding_box.min.y + offset.y) as u32,
                     // Turn the coverage into an alpha value
-                    Luma([color])
+                    Luma([color]),
                 )
             });
         }
     }
 }
 
-fn image_to_raster_lines(image: &image::GrayImage, width: u32) -> Vec<[u8; 90]> {
+pub fn image_to_raster_lines(image: &image::GrayImage, width: u32) -> Vec<[u8; 90]> {
     let width = width as usize;
     let line_count = image.len() / width;
 
@@ -94,12 +110,7 @@ fn image_to_raster_lines(image: &image::GrayImage, width: u32) -> Vec<[u8; 90]> 
             }
             image.get_pixel(0, 0);
             let luma_pixel = image.get_pixel(c as u32, r as u32); // + 3 was here in TS code -- not sure if needed
-            let value: u8 = if luma_pixel[0] > 0xFF / 2 {
-                0
-            }
-            else {
-                1
-            };
+            let value: u8 = if luma_pixel[0] > 0xFF / 2 { 0 } else { 1 };
             line[line_byte] |= value << line_bit_index;
         }
         lines.push(line);
@@ -120,7 +131,7 @@ impl TextRasterizer {
         Self {
             label,
             font_path,
-            second_row_image: None
+            second_row_image: None,
         }
     }
     /// Some types of label media (e.g. 12mm continuous tape) are wider than specified. Use this method to draw
@@ -134,7 +145,13 @@ impl TextRasterizer {
     /// as black on a white background. Enable the `invert` flag to print white text on a black background. Note that
     /// since the label is white, a faint border of white will still surround the label in areas that the printer
     /// cannot print the black background.
-    pub fn rasterize(&self, text: &str, secondary_text: Option<&str>, font_scale: f32, invert: bool) -> Vec<[u8; 90]> {
+    pub fn rasterize(
+        &self,
+        text: &str,
+        secondary_text: Option<&str>,
+        font_scale: f32,
+        invert: bool,
+    ) -> Vec<[u8; 90]> {
         let font_data = fs::read(&self.font_path).expect("Invalid font path");
         let font: Font<'static> = Font::from_bytes(font_data).unwrap();
 
@@ -150,12 +167,11 @@ impl TextRasterizer {
                 // 12mm label seems to need this for some reason
                 width += 25;
                 // 12mm labels have a second label below the primary that can actually be used
-				if self.second_row_image.is_some() {
-					secondary_width = 170;
-				}
+                if self.second_row_image.is_some() {
+                    secondary_width = 170;
+                }
             }
-        }
-        else {
+        } else {
             // Die cut labels
             width = self.label.dots_printable.0 + self.label.right_margin as u32;
             length = self.label.dots_printable.1;
@@ -166,17 +182,13 @@ impl TextRasterizer {
         for (_x, y, pixel) in image.enumerate_pixels_mut() {
             if invert {
                 let top_label_size =
-                    self.label.dots_printable.0
-                    + self.label.right_margin as u32
-                    + 15;
+                    self.label.dots_printable.0 + self.label.right_margin as u32 + 15;
                 if y > top_label_size {
                     *pixel = Luma([255]); // Set to white
-                }
-                else {
+                } else {
                     *pixel = Luma([0]); // Set to black
                 }
-            }
-            else {
+            } else {
                 *pixel = Luma([255]); // Set to white
             }
         }
@@ -184,25 +196,26 @@ impl TextRasterizer {
         match secondary_text {
             Some(secondary_text) => {
                 let primary = ResizedText::create(&font, text, length, 90.0 * font_scale);
-                let secondary = ResizedText::create(&font, secondary_text, length, 35.0 * font_scale);
+                let secondary =
+                    ResizedText::create(&font, secondary_text, length, 35.0 * font_scale);
 
                 let primary_offset = XY {
                     x: (length as i32 / 2) - (primary.rendered_size.x as i32 / 2),
-                    y: (width  as i32 / 2) - (primary.rendered_size.y as i32 / 2) - 25,
+                    y: (width as i32 / 2) - (primary.rendered_size.y as i32 / 2) - 25,
                 };
                 let secondary_offset = XY {
                     x: (length as i32 / 2) - (secondary.rendered_size.x as i32 / 2),
-                    y: (width  as i32 / 1) - (secondary.rendered_size.y as i32 / 2) - 20,
+                    y: (width as i32 / 1) - (secondary.rendered_size.y as i32 / 2) - 20,
                 };
                 draw_glyphs(&mut image, &primary.glyphs, primary_offset, invert);
                 draw_glyphs(&mut image, &secondary.glyphs, secondary_offset, invert);
-            },
+            }
             None => {
                 let primary = ResizedText::create(&font, text, length, 125.0 * font_scale);
 
                 let offset = XY {
                     x: (length as i32 / 2) - (primary.rendered_size.x as i32 / 2) - 5,
-                    y: (width  as i32 / 2) - (primary.rendered_size.y as i32 / 2),
+                    y: (width as i32 / 2) - (primary.rendered_size.y as i32 / 2),
                 };
 
                 draw_glyphs(&mut image, &primary.glyphs, offset, invert);
@@ -221,7 +234,12 @@ impl TextRasterizer {
                 new_height = secondary_width - top_margin;
                 new_width = (new_height as f32 * ratio) as u32;
             }
-            let resized = image::imageops::resize(&overlay, new_width, new_height, image::FilterType::Triangle);
+            let resized = image::imageops::resize(
+                &overlay,
+                new_width,
+                new_height,
+                image::FilterType::Triangle,
+            );
             image::imageops::overlay(&mut image, &resized, (length - new_width) / 2, width);
         }
 
